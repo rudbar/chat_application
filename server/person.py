93b63cfda6abcdd092d593612bd55de787b3bb96
Ian Kachan <kachan.yanf@gmail.com>
2020-04-15 22:58:38 +0500

    Slightly modified person.py and straighten up the code server.py

--- server/person.py
+++ server/person.py
@@ -1,8 +1,11 @@
 class Person:
-    def __init__(self, addr, name, client):
+    def __init__(self, addr, client):
         self.addr = addr
-        self.name = name
         self.client = client
+        self.name = None
+
+    def set_name(self, name, ):
+        self.name = name
 
     def __repr__(self):
         return f"Person({self.addr}, {self.name})"
--- server/server.py
+++ server/server.py
@@ -1,53 +1,78 @@
 from socket import AF_INET, socket, SOCK_STREAM 
 from threading import Thread
 import time
+from person import Person
 
-# ГЛОБАЛЬНЫЕ ПЕРЕМЕННЫЕ
+# ГЛОБАЛЬНЫЕ КОНСТАНТЫ
 HOST = 'localhost'
 PORT = 5500
 ADDR = (HOST, PORT)
 MAX_CONNECTIONS = 10
 BUFSIZ = 512
 
-def broadcast():
+# ГЛОБАЛЬНЫЕ ПЕРЕМЕННЫЕ
+persons = []
+SERVER = socket(AF_INET, SOCK_STREAM)
+SERVER.bind(ADDR) # настройка сервера
 
-def client_communication(client):
-    """
-    Поток обрабатывает все сообщения от клиента
-    :param client: socket
-    :return: None
-    """
-    run = True
-    while run:
-        msg = client.recv(BUFSIZ)
-        if msg == bytes("{quit}", "utf8"):
-            client.close()
+
+def broadcast(msg, name):
+    # отправляем новые сообщения всем пользователям
+    for person in persons:
+        client = person.client
+        client.send(bytes(name + ": ", "utf8") + msg)
+
+
+def client_communication(person):
+    # Поток обрабатывает все сообщения от пользователя
+ 
+    client = person.client
+
+    # получаем имя пользователя
+    name = client.recv(BUFSIZ).decode("utf8")
+    msg = f"{name} присоединился к беседе!"
+    broadcast(msg) # вывод сообщения приветствия
+
+    while True:
+        try:
+            msg = client.recv(BUFSIZ)
+            print(f"{name}: ", msg.decode("utf8"))
+
+            if msg == bytes("{quit}", "utf8"):
+                broadcast(f"{name} покинул чат...", "")
+                client.send(bytes("{quit}", "utf8"))
+                client.close()
+                persons.remove(person)
+                break
+            else:
+                broadcast(msg, name)
+        except Exception as e:
+            print("[ОШИБКА]", e)
+            break
+
 
 def wait_for_connection():
-    """
-    Ожидание подключения нового клиента, когда новый клиент подключился, 
-    запускается новый поток
-    :param SERVER: socket
-    :return: None
-    """
+    # Ожидание подключения нового пользователя, когда новый пользователь подключился, 
+    # запускается новый поток
+
     run = True
     while run:
         try:
             client, addr = SERVER.accept()
-            print(f"[CONNECTION] {addr} connected to the server at {time.time().now}")
-            Thread(target=client_communication, args=(client,)).start()
+            person = Person(addr, client)
+            persons.append(person)
+            print(f"[ПОДКЛЮЧЕНИЕ] {addr} присоединился к беседе в {time.time().now}")
+            Thread(target=client_communication, args=(person,)).start()
         except Exception as e:
-            print("[FAILURE]", e)
+            print("[ОШИБКА]", e)
             run = False
 
-    print("SERVER CRASHED")
+    print("СБОЙ НА СЕРВЕРЕ")
 
-SERVER = socket(AF_INET, SOCK_STREAM)
-SERVER.bind(ADDR)
 
 if __name__ == "__main__":
     SERVER.listen(MAX_CONNECTIONS)
-    print("Waiting for connection...")
+    print("Ожидание подключения...")
     ACCEPT_THREAD = Thread(target=wait_for_connection)
     ACCEPT_THREAD.start()
     ACCEPT_THREAD.join()
